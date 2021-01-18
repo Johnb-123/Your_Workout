6b64654e51f25bc7673723cf77b99b2a1a41d36a
John Byrne <68011959+Johnb-123@users.noreply.github.com>
2021-01-18 10:41:21 +0000

    Add files via upload

---
+++ moves.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 08:52:59 2020
+
+@author: Workout_Database
+
+Idea is create a workout Database, so i can import the module and pull 
+movements from there. 
+"""
+
+
+begin = ('lunges', 'skipping', 'jumping jacks', 'air squats',
+               '100m run', 'situps', '30second plank', 
+               'kettlebell swings','tricep dips',
+               'shoulder press', 'push ups')
+
+inter = ('push ups', 'burpees', 'weighted squats', 
+               'deadlifts','push press', 'box jumps', '300m run', 
+               '50 cals Assault bike',
+               'pull ups', 'medicine ball slams', '1min plank')
+
+advan = ('power snatches', 'power cleans', 
+               'handstand push ups','muscle-ups', 'dips', '500m run',
+               'clean and jerks', 'heavy deadlift',
+               'pull ups', 'burpees', 'box jumps')
---
+++ reps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 08:57:56 2020
+
+@author: John Byrne
+
+Creation of times module in order to import it and make it concise. 
+"""
+import random 
+
+#rep scheme depending on level
+schemeA = random.randint(5,10) #Beginner rep scheme
+schemeB = random.randint(10,20) #Intermiate rep scheme
+schemeC = random.randint(20,30) #Advanced rep scheme
+
+#rounds depending on level.
+time_1 = 3
+time_2 = 5
+time_3 = 7
